e4b059c383b8e448645c5fed3dc2f81fca8906e1
b10t <b10t.rus@gmail.com>
2022-03-04 10:38:46 +0800

    Added the Links model.

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Links
+
+
+@admin.register(Links)
+class LinksAdmin(admin.ModelAdmin):
+    pass
--- links/models.py
+++ links/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Links(models.Model):
+    link = models.CharField(
+        max_length=300,
+        verbose_name='Ссылка')
+    short_link = models.CharField(
+        max_length=300,
+        verbose_name='Короткая ссылка')
+
+    def __str__(self) -> str:
+        return f'{self.link}'
+
+    class Meta:
+        verbose_name = 'Ссылка'
+        verbose_name_plural = 'Ссылки'
