058962f831650ff36656200b0bfad93e3c9a4a1a
b10t <b10t.rus@gmail.com>
2022-03-04 12:57:53 +0800

    Added an api application.

--- short_links/settings.py
+++ short_links/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
-    'links'
+    'rest_framework',
+    'links',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
